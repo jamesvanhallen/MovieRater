24a92746c1c7edb32191011d5736c463c49ac6b3
Илья Бублик <ilya.bublik@e-legion.com>
2019-06-03 12:20:41 +0300

    model fixed

--- app/src/main/kotlin/com/jamesvanhallen/movierater/model/database/movie/Movie.kt
+++ app/src/main/kotlin/com/jamesvanhallen/movierater/model/database/movie/Movie.kt
@@ -2,20 +2,12 @@ package com.jamesvanhallen.movierater.model.database.movie
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
-import androidx.room.Index
 import androidx.room.PrimaryKey
 import com.jamesvanhallen.movierater.model.database.MoviesDataBase.Companion.MOVIES_TABLE_NAME
 
-@Entity(
-    tableName = MOVIES_TABLE_NAME,
-    indices = [Index(
-        value = arrayOf("_id"),
-        name = "idx_id",
-        unique = true
-    )]
-)
+@Entity(tableName = MOVIES_TABLE_NAME)
 data class Movie(
-    @PrimaryKey @ColumnInfo(name = ID) val id: Long,
+    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = ID) val id: Long,
     @ColumnInfo(name = NAME) val name: String,
     @ColumnInfo(name = RATIO) var ratio: Float,
     @ColumnInfo(name = IMAGE) val image: String
